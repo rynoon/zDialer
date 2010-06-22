dfc6edc2684e790dbc8a3d3e75e7adf88d765bb8
Stephen Henderson <me@steveh.ca>
2010-06-22 18:33:06 -0400

    Basic Project Template

---
+++ src/ca/zdialer/Dialer.java
@@ -0,0 +1,13 @@
+package ca.zdialer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Dialer extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
